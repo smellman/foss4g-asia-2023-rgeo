0ae30bf23737c5497fe68ef7b5083cc4e241407a
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-11-21 09:58:25 +0900

    set mime_types

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :toilets
+  resources :toilets, defaults: { format: 'geojson' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
