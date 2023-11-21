a4052de75ea47b161010aa09f6c41f7d4538c130
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-11-21 09:25:23 +0900

    added seed_data

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+def seed_toilets
+    Rails.logger.info 'Seed toilets'
+    toilets_geojson = File.read('db/seed_data/toilets.geojson')
+    toilets = RGeo::GeoJSON.decode(toilets_geojson)
+    toilets.each do |toilet|
+        name = toilet.properties['name'] ? toilet.properties['name'] : 'no name'
+        Toilet.create(
+            name: name,
+            location: toilet.geometry
+        )
+    end
+end
+seed_toilets
