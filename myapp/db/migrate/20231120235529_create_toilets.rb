063c3d73416af0cf4c71f66ab5e71471fb5a7cbe
Taro Matsuzawa <btm@tech.email.ne.jp>
2023-11-21 09:10:56 +0900

    db:migrate

--- myapp/db/migrate/20231120235529_create_toilets.rb
+++ myapp/db/migrate/20231120235529_create_toilets.rb
@@ -1,6 +1,8 @@
 class CreateToilets < ActiveRecord::Migration[7.0]
   def change
     create_table :toilets do |t|
+      t.string :name
+      t.st_point :location, geographic: true
 
       t.timestamps
     end
